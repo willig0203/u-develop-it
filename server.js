278fc6e46ba0e571ebaea29943d5d3ecb5249a34
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-07-14 16:33:12 -0500

    post candidate fixed and working

--- server.js
+++ server.js
@@ -82,23 +82,23 @@ app.delete('/api/candidate/:id', (req, res) => {
 app.post('/api/candidate', ({ body }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
     if (errors) {
-        const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
-  VALUES (?,?,?)`;
-        const params = [body.first_name, body.last_name, body.industry_connected];
-
-        db.query(sql, params, (err, result) => {
-            if (err) {
-                res.status(400).json({ error: err.message });
-                return;
-            }
-            res.json({
-                message: 'success',
-                data: body
-            });
-        });
         res.status(400).json({ error: errors });
         return;
     }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
 });
 
 
